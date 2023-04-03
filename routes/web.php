c1564bef88797659d960dc260a99ae0074a1a526
Tiago Bizze <Tiago Bizze>
2023-04-03 14:45:55 -0300

    comit da breeze com fortfy

--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,9 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+Route::get('/test', function () {
+    return Inertia::render('Test');
+});
+
 require __DIR__.'/auth.php';
